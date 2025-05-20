b1dd0cb1abc889fd2e3877b665aed9e240c997c2
Maksim Matusevich <>
2025-05-20 15:08:13 -0700

    initial commit

---
+++ android/app/src/main/kotlin/com/maximm/watchos_demo/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.maximm.watchos_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ ios/watchOSProj Watch App/watchOSProjApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchOSProjApp.swift
+//  watchOSProj Watch App
+//
+//  Created by Maksim Matusevich on 5/20/25.
+//
+
+import SwiftUI
+
+@main
+struct watchOSProj_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
